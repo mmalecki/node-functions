c9fe157ccea71189bac4b48dec3c355ff8f2673c
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-10-17 17:01:22 +0200

    [refactor test] DRY tests

--- test/functions-test.js
+++ test/functions-test.js
@@ -8,12 +8,12 @@ var N = 1024; // this is a bit silly
 var OurError = function () {};
 util.inherits(OurError, Error);
 
-vows.describe('functions').addBatch({
-  '`returns` function result': {
+function testFunction(func, tests) {
+  return {
     topic: function () {
-      return functions.returns(42);
+      return func;
       //
-      // This is cause by a bug/feature in vows. When vows is given a function
+      // This is caused by a bug/feature in vows. When vows is given a function
       // as a topic, it executes it. Since our functions return functions, this
       // behavior results in unexpected results and has to be prevented.
       //
@@ -21,33 +21,27 @@ vows.describe('functions').addBatch({
     'should be a function': function (topic) {
       assert.isFunction(topic);
     },
+    'when called': tests
+  };
+};
+
+vows.describe('functions').addBatch({
+  '`returns` function result': testFunction(functions.returns(42), {
     'should always return given value': function (topic) {
       for (var i = 0; i < N; i++) {
         assert.equal(topic(), 42);
       }
     }
-  },
-  '`throws` function result': {
-    topic: function () {
-      return functions.throws(new OurError('Expected error'));
-    },
-    'should be a function': function (topic) {
-      assert.isFunction(topic);
-    },
+  }),
+  '`throws` function result': testFunction(functions.throws(new OurError('Expected error')), {
     'should always throw given error': function (topic) {
       for (var i = 0; i < N; i++) {
         assert.throws(topic, OurError);
       }
     }
-  },
+  }),
   '`throwsIfError` function result': {
-    'without `exception` parameter': {
-      topic: function () {
-        return functions.throwsIfError();
-      },
-      'should be a function': function (topic) {
-        assert.isFunction(topic);
-      },
+    'without `exception` parameter': testFunction(functions.throwsIfError(), {
       "should always throw first parameter if it's truthy": function (topic) {
         for (var i = 0; i < N; i++) {
           assert.throws(topic.bind(topic, new OurError('Expected error')),
@@ -59,14 +53,8 @@ vows.describe('functions').addBatch({
           assert.doesNotThrow(topic.bind(topic, false));
         }
       }
-    },
-    'with `exception` parameter': {
-      topic: function () {
-        return functions.throwsIfError(new OurError('Expected error'));
-      },
-      'should be a function': function (topic) {
-        assert.isFunction(topic);
-      },
+    }),
+    'with `exception` parameter': testFunction(functions.throwsIfError(new OurError('Expected error')), {
       'should always throw provided error if first parameter is truthy': function (topic) {
         for (var i = 0; i < N; i++) {
           assert.throws(topic.bind(topic, true), OurError);
@@ -77,15 +65,9 @@ vows.describe('functions').addBatch({
           assert.doesNotThrow(topic.bind(topic, false), Error);
         }
       }
-    }
+    })
   },
-  '`noop` function': {
-    topic: function () {
-      return functions.noop;
-    },
-    'should be a function': function (topic) {
-      assert.isFunction(topic);
-    },
+  '`noop` function': testFunction(functions.noop, {
     'when called': {
       'should never return anything': function (result) {
         for (var i = 0; i < N; i++) {
@@ -93,78 +75,54 @@ vows.describe('functions').addBatch({
         }
       }
     }
-  },
-  '`eq` function': {
-    topic: function () {
-      return functions.eq;
+  }),
+  '`eq` function': testFunction(functions.eq, {
+    'with equal values': {
+      topic: functions.eq(42, 42),
+      'should return true': function (result) {
+        assert.isTrue(result);
+      }
     },
-    'should be a function': function (topic) {
-      assert.isFunction(topic);
-    },
-    'when called': {
-      'with equal values': {
-        topic: functions.eq(42, 42),
-        'should return true': function (result) {
-          assert.isTrue(result);
-        }
-      },
-      'with equalish values': {
-        topic: functions.eq('42', 42),
-        'should return true': function (result) {
-          assert.isTrue(result);
-        }
+    'with equalish values': {
+      topic: functions.eq('42', 42),
+      'should return true': function (result) {
+        assert.isTrue(result);
       }
     }
-  },
-  '`strictEq` function': {
-    topic: function () {
-      return functions.strictEq;
+  }),
+  '`strictEq` function': testFunction(functions.strictEq, {
+    'with equal values': {
+      topic: functions.strictEq(42, 42),
+      'should return true': function (result) {
+        assert.isTrue(result);
+      }
     },
-    'should be a function': function (topic) {
-      assert.isFunction(topic);
-    },
-    'when called': {
-      'with equal values': {
-        topic: functions.strictEq(42, 42),
-        'should return true': function (result) {
-          assert.isTrue(result);
-        }
-      },
-      'with equalish values': {
-        topic: functions.strictEq('42', 42),
-        'should return false': function (result) {
-          assert.isFalse(result);
-        }
+    'with equalish values': {
+      topic: functions.strictEq('42', 42),
+      'should return false': function (result) {
+        assert.isFalse(result);
       }
     }
-  },
-  '`gt` function': {
-    topic: function () {
-      return functions.gt;
+  }),
+  '`gt` function': testFunction(functions.gt, {
+    'with first argument greater than second': {
+      topic: functions.gt(42, -42),
+      'should return true': function (result) {
+        assert.isTrue(result);
+      }
     },
-    'should be a function': function (topic) {
-      assert.isFunction(topic);
+    'with first argument less than second': {
+      topic: functions.gt(42, 84),
+      'should return false': function (result) {
+        assert.isFalse(result);
+      }
     },
-    'when called': {
-      'with first argument greater than second': {
-        topic: functions.gt(42, -42),
-        'should return true': function (result) {
-          assert.isTrue(result);
-        }
-      },
-      'with first argument less than second': {
-        topic: functions.gt(42, 84),
-        'should return false': function (result) {
-          assert.isFalse(result);
-        }
-      },
-      'with equal arguments': {
-        topic: functions.gt(42, 42),
-        'should return false': function (result) {
-          assert.isFalse(result);
-        }
+    'with equal arguments': {
+      topic: functions.gt(42, 42),
+      'should return false': function (result) {
+        assert.isFalse(result);
       }
     }
-  }
+  })
 }).export(module);
 
